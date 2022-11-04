8a75b5aeb692e7e8ecad31ba3443131fe58f86e1
ryansldev <ryanlima1309@gmail.com>
2022-11-04 14:27:54 -0300

    refactor: change join poll route

--- src/routes/pool.ts
+++ src/routes/pool.ts
@@ -47,7 +47,7 @@ export async function poolRoutes(fastify: FastifyInstance) {
     return reply.status(201).send({ code });
   })
 
-  fastify.post("/pools/:id/join", {
+  fastify.post("/pools/join", {
     onRequest: [authenticate],
   }, async (request, reply) => {
     const joinPoolBody = z.object({
